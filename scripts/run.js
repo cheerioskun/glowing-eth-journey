7a78aceb115efdb204651046ba6c78c0bfa7ec87
Hemant Pandey <hpandey110301@gmail.com>
2022-02-10 15:55:19 +0530

    Initial commit: Setup basic hardhat template

---
+++ scripts/run.js
@@ -0,0 +1,21 @@
+
+
+main = async () => {
+    const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+    const waveContract = await waveContractFactory.deploy();
+    await waveContract.deployed();
+    console.log("Contract deployed to: ", waveContract.address);
+};
+
+
+const runMain = async () => {
+    try {
+        await main();
+        process.exit(0);
+    } catch (err) {
+        console.log(err);
+        process.exit(1);
+    }
+}
+
+runMain();
